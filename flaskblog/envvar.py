607f720ac7af2145e73ec12f39c88d1a3a57c3ae
Hrituja <hrkhatavkar@gmail.com>
2020-06-28 14:15:33 +0530

    initial commit

---
+++ flaskblog/envvar.py
@@ -0,0 +1,8 @@
+import os
+
+# db_email = 
+# db_password = 
+
+
+print(os.environ.get('EMAIL_USER'))
+print(os.environ.get('EMAIL_PASS'))
---
+++ run.py
@@ -0,0 +1,31 @@
+from flaskblog import app
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+#running flask
+'''
+export FLASK_APP=flaskblog.py
+export FLASK_DEBUG=1
+flask run
+
+or 
+
+if __name__ == '__main__':
+	app.run()
+
+and on the terminal : python3 flaskbog.py
+'''
+
+
+
